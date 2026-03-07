8cdacd1c05a2ba46fa3d190a45939b4335ed772f
Mikias <mikias1219@example.com>
2026-03-07 08:40:37 +0300

    Fix TypeScript type error in crypto.randomUUID polyfill

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -7,12 +7,14 @@ export function cn(...inputs: ClassValue[]) {
 
 // Polyfill for crypto.randomUUID for older browsers
 if (typeof crypto !== 'undefined' && !crypto.randomUUID) {
-  crypto.randomUUID = function() {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  crypto.randomUUID = function(): `${string}-${string}-${string}-${string}-${string}` {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       const r = Math.random() * 16 | 0;
       const v = c === 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
-    });
+    }) as `${string}-${string}-${string}-${string}-${string}`;
   };
 }
 
